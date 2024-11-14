25ab76cb23cc3990aba0f7dc5a0212e25030975d
Velospace <86271580+Velospace@users.noreply.github.com>
2024-11-13 22:44:08 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 
+const container = document.getElementById("container")
+
+container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+
+// When clicked, render an HTML paragraph under the button (in the container)
+// that says "Thank you for buying!"
