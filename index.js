0e7410f9c2904258c8d26916be0faae3d7e3d371
Velospace <86271580+Velospace@users.noreply.github.com>
2024-11-13 22:58:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+const container = document.getElementById("container");
 
-const container = document.getElementById("container")
+container.innerHTML = "<button onclick='buy()'>Buy!</button>";
 
-container.innerHTML = "<button onclick='buy()'>Buy!</button>"
+function buy() {
+    const p = document.createElement("p");
+    p.textContent = "Thank you for buying!";
+    
 
-// When clicked, render an HTML paragraph under the button (in the container)
-// that says "Thank you for buying!"
+    container.appendChild(p);
+}
